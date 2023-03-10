659bf9dc901b0b3634f5dec73e639cbf97189e5a
Bob Faist <bob.faist@gmail.com>
2023-03-09 21:14:00 -0500

    Initial Commit

---
+++ Book Barcode Scanner/Book_Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Book_Barcode_ScannerApp.swift
+//  Book Barcode Scanner
+//
+//  Created by Robert Faist on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Book_Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
